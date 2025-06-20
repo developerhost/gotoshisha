833f7af08ab9f303c0caf4a71108e65be8db9060
developerhost <kanonhara0609@gmail.com>
2025-06-20 18:19:21 +0900

    feat: Auth0の設定で環境変数が未設定の場合にエラーをスローするように変更

--- app/config/auth0.ts
+++ app/config/auth0.ts
@@ -1,5 +1,16 @@
 export const auth0Config = {
-  domain: process.env.EXPO_PUBLIC_AUTH0_DOMAIN,
-  clientId: process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID,
-  scheme: "gotoshisha",
+  domain:
+    process.env.EXPO_PUBLIC_AUTH0_DOMAIN ||
+    (() => {
+      throw new Error(
+        "EXPO_PUBLIC_AUTH0_DOMAIN environment variable is required"
+      );
+    })(),
+  clientId:
+    process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID ||
+    (() => {
+      throw new Error(
+        "EXPO_PUBLIC_AUTH0_CLIENT_ID environment variable is required"
+      );
+    })(),
 };
