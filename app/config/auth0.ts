eea595b86333050b8f5eebd2c2d4de746a35dc7b
developerhost <kanonhara0609@gmail.com>
2025-06-20 17:28:44 +0900

    feat: Auth0のスキームを'expo-react-native-maps-demo'から'gotoshisha'に変更

--- app/config/auth0.ts
+++ app/config/auth0.ts
@@ -1,5 +1,5 @@
 export const auth0Config = {
   domain: 'dev-cz7g2cer3i7mpz25.jp.auth0.com',
   clientId: process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID || '',
-  scheme: 'expo-react-native-maps-demo'
+  scheme: 'gotoshisha'
 };
