99ae26ddb1de128898634f6527209e06b23ecc55
developerhost <kanonhara0609@gmail.com>
2025-06-20 14:52:49 +0900

    feat: QueryProviderからReact Query DevToolsを削除

--- app/providers/QueryProvider.web.tsx
+++ app/providers/QueryProvider.web.tsx
@@ -3,7 +3,6 @@
  */
 import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "../lib/queryClient";
 
 interface QueryProviderProps {
@@ -12,13 +11,6 @@ interface QueryProviderProps {
 
 export const QueryProvider: React.FC<QueryProviderProps> = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools
-        initialIsOpen={false}
-        buttonPosition="bottom-right"
-        position="bottom"
-      />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 };
