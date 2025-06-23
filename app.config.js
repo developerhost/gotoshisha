7e2a5eebd2fa56a160e7d228403191440f55f1a9
waki285 <suzuneu@suzuneu.com>
2025-06-23 20:56:48 +0900

    plugin 分離

--- app.config.js
+++ app.config.js
@@ -1,16 +1,7 @@
-import { GOOGLE_API_KEY } from "react-native-dotenv";
-
 export default ({ config }) => {
   return {
     plugins: [
       "expo-router",
-      /*[
-        "react-native-maps",
-        {
-          iosGoogleMapsApiKey: GOOGLE_API_KEY,
-          androidGoogleMapsApiKey: GOOGLE_API_KEY,
-        },
-      ],*/
     ],
     ...config,
   };
